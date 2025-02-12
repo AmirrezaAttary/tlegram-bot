4d3e60a28e030f085c57320ff295a065c583e69f
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2025-02-12 20:02:15 +0330

    ok

--- core/main.py
+++ core/main.py
@@ -1,8 +1,19 @@
 import telebot
 import os
+import pprint
+import json
 
 API_TOKEN = os.environ.get('API_TOKEN')
 
 bot = telebot.TeleBot(API_TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message.chat.id,json.dumps(message.chat.id.__dict__,indent=4,check_circular=False))
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
 
 bot.infinity_polling()
